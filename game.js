15c453feb05d1db53bf6452d460ca9723ab5e614
Jerrold Spencer <2abc2xyz7a6b@gmail.com>
2019-08-19 16:55:45 -0600

    push for the day

--- game.js
+++ game.js
@@ -1,6 +1,12 @@
-let health = 100;
-let hits = 0;
 
+let health = 100
+let hits = 0
+
+let items = {
+  fire: { name: 'Fire', modifier: 2, description: 'IT BURNS!' },
+  potion: { name: 'Potion', modifier: 4, description: 'I feel better' },
+  shield: { name: 'Shield', modifier: 2, description: 'Leave me alone' }
+}
 
 function slap() {
   if (health <= 100) {
